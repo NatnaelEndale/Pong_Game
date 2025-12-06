74aba70dd6eb4eb29f5b672e80cd26bb0300ed0d
Natnael Endale Cheru <natnaelendale22@gmail.com>
2025-12-06 16:46:24 +0300

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.shapesize(0.8, 0.8)
+        self.penup()
+        # self.goto(-280, 0)
+
+    def move(self, x_direction, y_direction):
+        new_x = self.xcor() + x_direction
+        new_y = self.ycor() + y_direction
+        self.goto(new_x, new_y)
+
+    # def change_vertical(self, direction):
+    #     new_x = self.xcor() - direction
+    #     new_y = self.ycor() + 10
+    #     self.goto(new_x,  new_y)
